e766bab5a269b547c6399540cab4707f1022e0d4
Taha Bouhsine <contact@tahabouhsine.com>
2026-04-20 07:09:23 -0700

    added yaml and removed old code

--- old landing page/blogs/js/information/main.js
+++
@@ -1,9 +0,0 @@
-import { initVizEntropy } from './viz-entropy.js';
-import { initVizGrounding } from './viz-grounding.js';
-import { initVizOmnilingual } from './viz-omnilingual.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initVizEntropy();
-    initVizGrounding();
-    initVizOmnilingual();
-});
